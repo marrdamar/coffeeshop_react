2a74c4a04734d7a63e2b9aeb486de23a7ebdd2e8
digi-squad <digitalsquadfw14@proton.me>
2023-06-12 09:01:10 +0700

    fixing private route

--- src/utils/https/promos.js
+++ src/utils/https/promos.js
@@ -8,7 +8,5 @@ const baseUrl = process.env.REACT_APP_SERVER_HOST;
 
 export const getPromos = (controller) => {
   const url = baseUrl + "/promos";
-  const storeToken = store.getState();
-  const token = storeToken.user.token;
   return axios.get(url, { signal: controller.signal });
 };
